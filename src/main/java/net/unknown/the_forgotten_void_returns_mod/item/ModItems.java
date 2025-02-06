40d8aa595a6d78e808bec5732cbdcf31e400ae96
MQDMF <mqayyumdarwisyfirdaus@gmail.com>
2025-02-06 14:21:23 +0800

    changes

--- src/main/java/net/unknown/the_forgotten_void_returns_mod/item/ModItems.java
+++ src/main/java/net/unknown/the_forgotten_void_returns_mod/item/ModItems.java
@@ -1,8 +1,10 @@
 package net.unknown.the_forgotten_void_returns_mod.item;
 
 import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
 import net.unknown.the_forgotten_void_returns_mod.The_Forgotten_Void_Returns_Mod;
 
 public class ModItems {
@@ -10,5 +12,10 @@ public class ModItems {
             DeferredRegister.create(ForgeRegistries.ITEMS, The_Forgotten_Void_Returns_Mod.MOD_ID);
 
 
+    public static final RegistryObject<Item> VoidStone =  ITEMS.register("voidstone",
+            () -> new Item(new Item.Properties()));
 
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
 }
