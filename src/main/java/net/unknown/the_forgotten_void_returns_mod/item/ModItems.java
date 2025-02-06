0011b24d3426195ff0e691b32cee11bb15f633e6
MQDMF <mqayyumdarwisyfirdaus@gmail.com>
2025-02-06 13:26:59 +0800

    changes

---
+++ src/main/java/net/unknown/the_forgotten_void_returns_mod/item/ModItems.java
@@ -0,0 +1,14 @@
+package net.unknown.the_forgotten_void_returns_mod.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.unknown.the_forgotten_void_returns_mod.The_Forgotten_Void_Returns_Mod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, The_Forgotten_Void_Returns_Mod.MOD_ID);
+
+
+
+}
